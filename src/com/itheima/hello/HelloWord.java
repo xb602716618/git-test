ece2e6ae9674dd9049be80143f7e82963ebbc780
xb <xb961121@163.com>
2019-02-17 21:17:30 +0800

    添加

--- src/com/itheima/hello/HelloWord.java
+++ src/com/itheima/hello/HelloWord.java
@@ -5,4 +5,10 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("githup");
     }
+
+
+
+    public void test(){
+        System.out.println("++++++++++");
+    }
 }
